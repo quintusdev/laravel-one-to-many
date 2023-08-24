5b598d102056c4462054b844128410ebef63d801
quintusdev <126263354+quintusdev@users.noreply.github.com>
2023-08-24 14:10:36 +0200

    [FEATURES] Gestione errori type id Store Pot

--- app/Http/Requests/StorePostRequest.php
+++ app/Http/Requests/StorePostRequest.php
@@ -25,7 +25,8 @@ class StorePostRequest extends FormRequest
     {
         return [
             'title' => 'required|max:50',
-            'image' => 'image|max:250'
+            'image' => 'image|max:250',
+            'type_id' => 'required|exists:types,id'
         ];
     }
     /* Funzione per gestire i messaggi di errore in ITA */
@@ -36,7 +37,10 @@ class StorePostRequest extends FormRequest
             'title.max'  => "Il titolo deve avere meno di :max caratteri.",
             /* messaggi di errore per l'immagine */
             'image.image'  => "Il file deve avere una delle seguenti estensioni: jpg, jpeg, png, webp",
-            'image.max'  => "L'indirizzo dell'immagine deve avere al massimo :max caratteri."
+            'image.max'  => "L'indirizzo dell'immagine deve avere al massimo :max caratteri.",
+            /* messaggi di errore per la tipologia */
+            'type_id.required' => "Devi selezionare una categoria",
+            'type_id.exists' => "Categoria selezionata non valida"
         ];
     }
 }
