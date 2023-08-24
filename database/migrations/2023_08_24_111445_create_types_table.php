c4284c9cc5d56fd7166953da73d94f451f37d766
quintusdev <126263354+quintusdev@users.noreply.github.com>
2023-08-24 13:20:25 +0200

    [FEATURES] inserimento dati controller e migration

--- database/migrations/2023_08_24_111445_create_types_table.php
+++ database/migrations/2023_08_24_111445_create_types_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('types', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 30);
+            $table->string('slug');
             $table->timestamps();
         });
     }
