08eca3c94fb8062b9507bfb838476a965a0d1c71
quintusdev <126263354+quintusdev@users.noreply.github.com>
2023-08-24 13:55:05 +0200

    [FEATURES] Funzione in post model per type

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,12 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 /* dico che può usare la STR per lo slug */
 use Illuminate\Support\Str;
 
+use App\Models\Type;
+
 class Post extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'content', 'image', 'slug'];
+    protected $fillable = ['title', 'content', 'image', 'slug', 'type_id'];
 
+    public function type()
+    {
+        return $this->belongsTo(Type::class); //relazione uno a molti tra post e tipo di post
+    }
     /* faccio la funzione che crea lo slug del titolo */
     public static function generateSlug($title)
     {
